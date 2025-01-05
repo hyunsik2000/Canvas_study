8804aa2b57d326e8dfc32940805c9e17f1579682
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2025-01-05 21:24:37 +0900

    [ADD] 클래스 활용 / 애니메이트 함수 실행

--- Canvas/Particle/index.js
+++ Canvas/Particle/index.js
@@ -1,7 +1,5 @@
 const canvas = document.querySelector('canvas')
-
 const ctx = canvas.getContext('2d')
-
 const dpr = window.devicePixelRatio 
 //dpr이 1 이면 1px에 정사각형 1개가 들어가 있는 그림
 //dpr이 2 이면 1px에 정사각형 4개가 들어가 있는 그림 <-- 더 선명함함 
@@ -16,3 +14,33 @@ canvas.width = canvasWidth * dpr
 canvas.height = canvasHeight * dpr
 ctx.scale(dpr,dpr)
 //따라서 canvas 기존 가로 세로 값에 dpr을 곱하고 ctx.scale(dpr,dpr)
+
+class Particle {
+    constructor(x, y, radius) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+    }
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI / 180 * 360)
+        ctx.fillStyle = 'red'
+        ctx.fill()
+        ctx.closePath()
+    }
+}
+
+const x = 100
+const y = 100
+const radius = 50
+const particle = new Particle(x,y,radius)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    particle.draw()
+}
+
+animate()
