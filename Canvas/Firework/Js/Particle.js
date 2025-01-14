ef06c9844fbb18aee20b6cbaa83514277d3354df
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2025-01-14 11:06:47 +0900

    [ADD] Math. sin cos 활용 원으로 퍼지게하기

--- Canvas/Firework/Js/Particle.js
+++ Canvas/Firework/Js/Particle.js
@@ -20,7 +20,7 @@ export default class Particle extends CanvasOption {
     draw() {
         this.ctx.fillStyle = `rgba(255,255,255, ${this.opacity})`
         this.ctx.beginPath()
-        this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2)
+        this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2)
         this.ctx.fill()
         this.ctx.closePath()
     }
