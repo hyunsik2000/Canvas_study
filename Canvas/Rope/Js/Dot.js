46a3c0f1293404bb52cce709f15fcc84256db55e
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2025-01-17 16:46:38 +0900

    [ADD] Vector 클래스 적용 및 static 메소드

---
+++ Canvas/Rope/Js/Dot.js
@@ -0,0 +1,12 @@
+export default class Dot {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+    update() {
+
+    }
+    draw() {
+
+    }
+}
--- Canvas/Rope/index.js
+++ Canvas/Rope/index.js
@@ -1,6 +1,16 @@
 import App from "./Js/App.js"
+import Vector from "./Js/Vector.js"
 
 window.addEventListener('load', ()=> {
     const app = new App()
     app.render()
 })
+
+const v1 = new Vector(100,100)
+const v2 = new Vector(50,50)
+
+
+
+const v3 = Vector.add(v1,v2)
+
+console.log(v3)
