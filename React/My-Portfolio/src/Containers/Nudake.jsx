00609c3f9674c07f497af0420aa222bfc197109d
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2025-01-19 09:18:35 +0900

    [ADD] globalCompositeOperation 적용

--- React/My-Portfolio/src/Containers/Nudake.jsx
+++ React/My-Portfolio/src/Containers/Nudake.jsx
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
+import lodash from 'lodash/throttle';
 import "../Style/Containers/Nudake.css";
+import image1 from "../Asset/newjeans1.jpg";
+import image2 from "../Asset/newjeans2.jpg";
+import image3 from "../Asset/newjeans3.jpg";
+import { getDistance, getAngle, getScrupedPercent } from "../Utils/Utils";
+import throttle from "lodash/throttle";
 
 const Nudake = () => {
     const canvasRef = useRef(null)
@@ -8,6 +14,10 @@ const Nudake = () => {
         const canvas = canvasRef.current
         const canvasParent = canvas.parentNode
         const ctx = canvas.getContext('2d')
+
+        const imageSrcs = [image1, image2, image3]
+        let imageIndex = 0
+        let prevPos = { x: 0, y: 0}
 
         let canvasWidth, canvasHeight
 
@@ -18,22 +28,75 @@ const Nudake = () => {
             canvas.style.height = canvasHeight + "px"
             canvas.width = canvasWidth
             canvas.height = canvasHeight
+
+            drawImage()
         }
 
-        let frameId
+        function drawImage(){
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+            const image = new Image()
+            image.src = imageSrcs[imageIndex]
+            image.onload = () => {
+            ctx.globalCompositeOperation = 'source-over' //globalCompositeOperation 에 대해 문서를 한번 훑어보자
+            ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight)
 
-        function frame() {
-            frameId = requestAnimationFrame(frame)
-            ctx.fillRect(100, 100, 100, 100)
+            const nextImage = imageSrcs[(imageIndex + 1) % imageSrcs.length]
+            canvasParent.style.backgroundImage = `url(${imageSrcs[nextImage]})`
+            }
         }
-        
+
+        function onMouseDown(e) {
+            console.log("onMouseDown")
+            window.addEventListener('mouseup', onMouseUp)
+            window.addEventListener('mousedown', onMouseDown)
+            prevPos = {x: e.offsetX, y: e.offsetY}
+        }
+        function onMouseUp() {
+            console.log("onMouseUp")
+            window.removeEventListener('mouseup', onMouseUp)
+            window.removeEventListener('mousedown', onMouseDown)
+        }
+        function onMouseMove(e) {
+            console.log("onMouseMove")
+            drawCircles(e)
+            checkPercent()
+        }
+
+        function drawCircles(e) {
+            const nextPos = { x: e.offsetX, y: e.offsetY}
+            const dist = getDistance(prevPos, nextPos)
+            const angle = getAngle(prevPos, nextPos)
+
+            for(let i = 0; i < dist; i++){
+                const x = prevPos.x + Math.cos(angle) * i
+                const y = prevPos.y + Math.sin(angle) * i
+
+            ctx.globalCompositeOperation = 'destination-out' //globalCompositeOperation 에 대해 문서를 한번 훑어보자
+            ctx.beginPath()
+            ctx.arc(x,y , canvasWidth / 15, 0, Math.PI * 2)
+            ctx.fill()
+            ctx.closePath()
+        }
+
+            prevPos = nextPos
+        }
+
+        const checkPercent = throttle(() => {
+            const percent = getScrupedPercent(ctx, canvasWidth, canvasHeight)
+            if (percent > 50){
+                imageIndex  = (imageIndex + 1) % imageSrcs.length
+                drawImage()
+            }
+        }, 500)
+
+        window.addEventListener('mousemove', onMouseMove)
         window.addEventListener('resize', resize) // 얘도 cleanUp 필수수
         resize()
-        requestAnimationFrame(frame) //리액트에서 이런식으로 frame 사용하면 컴포넌트가 언마운트 되도 계속 사용됨 -> cleanUp 필수수
+  
         
         return () => {
             window.removeEventListener('resize', resize)
-            cancelAnimationFrame(frameId)
+            window.removeEventListener('mousemove', onMouseMove)
         }
     },[])
 
---
+++ React/My-Portfolio/src/Utils/Utils.jsx
@@ -0,0 +1,28 @@
+export function getDistance(p1,p2) {
+    const dx = p2.x - p1.x
+    const dy = p2.y - p1.y
+
+    return Math.sqrt(dx * dx + dy * dy)
+}
+
+export function getAngle(p1,p2){
+    const dx = p2.x - p1.x
+    const dy = p2.y - p2.y
+
+    return Math.atan2(dy, dx)
+}
+
+export function getScrupedPercent(ctx, width, height){
+    const pixels = ctx.getImageData(0, 0, width, height)
+    const gap = 32
+    const total = pixels.data.length / gap
+    let count = 0
+
+    for(let i = 0; i < pixels.data.length - 3; i += gap){
+        if (pixels.data[i + 3] === 0){
+            count++
+        } 
+    }
+
+    return Math.round(count / total * 100)
+}
